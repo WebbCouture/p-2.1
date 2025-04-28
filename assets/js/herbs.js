2c3d53ad66e7be32c67bab98d67858ccb3b7071b
AmandaLesnik <lesnik.amanda@icloud.com>
2025-04-28 20:02:26 +0200

    change size text in box

--- assets/js/herbs.js
+++ assets/js/herbs.js
@@ -8,8 +8,8 @@
     ],
     hairHerbs: [
       { title: "Test1", benefits: "Test2", imageUrl: "https://unsplash.com/photos/brown-and-blue-hair-brush-edL3qKlrrr0" },
-      { title: "Lavender", benefits: "Promotes hair growth", imageUrl: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" },
-      { title: "Peppermint", benefits: "Stimulates scalp", imageUrl: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" }
+      { title: "wow1", benefits: "wow2", imageUrl: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" },
+      { title: "koko1", benefits: "koko2", imageUrl: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" }
     ],
     brainHerbs: [
       { title: "Skullcap", benefits: "Cleans and strengthens hair", imageUrl: "https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
