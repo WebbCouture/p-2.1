810ecbdb96d32c4785c523e1e1763f04d0932f79
AmandaLesnik <lesnik.amanda@icloud.com>
2025-04-29 13:32:44 +0200

    colors info

--- assets/js/beauty.js
+++ assets/js/beauty.js
@@ -1,37 +1,32 @@
 
   // beauty.js
   export const herbData = {
-    eyebeauty: [
-      { title: "Eyebright", benefits: "Clears mucus in the tear ducts", imageUrl: "https://images.unsplash.com/photo-1504382103100-db7e92322d39?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-      { title: "Chamomile", benefits: "Reduces eye inflammation", imageUrl: "https://plus.unsplash.com/premium_photo-1675441003177-1dbcf11fa772?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-      { title: "Bilberry", benefits: "Improves night vision", imageUrl: "https://images.unsplash.com/photo-1658509565784-d425ada6c327?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
-    ],
     hairbeauty: [
       { title: "Products", benefits: "Hair treatment", imageUrl: "https://media.istockphoto.com/id/1074797524/sv/foto/bild-av-showcase-med-flaskor-med-schampo-och-balsam.jpg?s=2048x2048&w=is&k=20&c=hOo93bzAhSO4gQ6YtTp0N8B3c_B_vGpowOCf8il8P1o=" },
       { title: "Refill bottles", benefits: "Own design", imageUrl: "https://plus.unsplash.com/premium_photo-1680853621273-c5e174898f1f?q=80&w=1957&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
       { title: "Hair treatmend", benefits: "Waching products", imageUrl: "https://plus.unsplash.com/premium_photo-1661672325251-683e6f7dea00?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
     ],
-    brainbeauty: [
+    scissorsbeauty: [
       { title: "Scissors", benefits: "Origninal", imageUrl: "https://images.unsplash.com/photo-1696091997052-f52f85285d8d?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
       { title: "Scissors", benefits: "Thin oute", imageUrl: "https://images.unsplash.com/photo-1589876518759-5b460cabcc4c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
       { title: "Treatment", benefits: "Scissor oil", imageUrl: "https://images.unsplash.com/photo-1515377905703-c4788e51af15?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
     ],
-    teethbeauty: [
+    makeupbeauty: [
       { title: "Makeup", benefits: "Natural mineral makeup", imageUrl: "https://media.istockphoto.com/id/1658893205/sv/foto/make-up-products-at-pink-background-top-view.jpg?s=2048x2048&w=is&k=20&c=R4A4iuQefwdoDZYk0Zz97s17vuV-B48a4ngphT0cL0E=" },
       { title: "Bruches", benefits: "Made with care", imageUrl: "https://media.istockphoto.com/id/1358832083/sv/foto/paintbrushes-on-a-pink-background-glamorous-makeup-artist-table-brushes-for-applying-powder.jpg?s=2048x2048&w=is&k=20&c=SBd_OTJx4hDwYoLgTkCre8vMM1EfuLW4BJrhn_Dg5-s=" },
       { title: "Skincare", benefits: "For sensitive scin", imageUrl: "https://media.istockphoto.com/id/1688862517/sv/foto/pink-skin-care-routine-products.jpg?s=2048x2048&w=is&k=20&c=4HSresG5r5I9eHPqqcaFZgJn0Gq1hLVVkXrirVHYiLQ=" }
     ],
-    heartbeauty: [
+    powertoolsbeauty: [
       { title: "Hairdryer", benefits: "Powerful and quiet", imageUrl: "https://images.unsplash.com/photo-1715220169023-c1d5c8d2be37?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
       { title: "Hair trimmer", benefits: "Best on the market", imageUrl: "https://images.unsplash.com/photo-1666622834106-4559bb3c76a3?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
       { title: "Barber", benefits: "Knife and more", imageUrl: "https://media.istockphoto.com/id/640276164/sv/foto/professional-barber-getting-rid-of-male-stubble.jpg?s=2048x2048&w=is&k=20&c=Zgztwo8Zph_6WpuQKUo2OU-DQHGrOrRFa-6aYDxcQ7E=" }
     ],
-    lungbeauty: [
-      { title: "Mugwort", benefits: "Cleans lungs", imageUrl: "https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-      { title: "Mullein Leaf", benefits: "Antiviral and Antibacterial properties", imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6" },
-      { title: "Eucalyptus", benefits: "Relieves congestion", imageUrl: "https://plus.unsplash.com/premium_photo-1673264303561-de2ab31df03c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+    colorsbeauty: [
+      { title: "Colors", benefits: "Natural colors", imageUrl: "https://media.istockphoto.com/id/175725740/sv/foto/highlight-hair-texture-background.jpg?s=2048x2048&w=is&k=20&c=XEB74rhwEYe7kK2S2tKedKeQJnlzZvFDLym7VYil9jg=" },
+      { title: "Lightening", benefits: "Bleach", imageUrl: "https://media.istockphoto.com/id/1307578256/sv/foto/process-f%C3%B6r-applicering-av-blekpulver-p%C3%A5-klienter-h%C3%A5r-och-omslag-i-folien-shatush-teknik.jpg?s=2048x2048&w=is&k=20&c=jp7wz_r1uIgMjp8NeTqTBWgrTjmUUrbI1LTwtmC-FFY=" },
+      { title: "Crasy colors", benefits: "Dear to be colorful", imageUrl: "https://images.unsplash.com/photo-1546561925-a427a021303a?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
     ],
-    digestionbeauty: [
+    interiorbeauty: [
       { title: "Seeds", benefits: "Cleans lungs", imageUrl: "https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
       { title: "Irish Moss", benefits: "Antiviral and Antibacterial properties", imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6" },
       { title: "Dillsk", benefits: "Relieves congestion", imageUrl: "https://plus.unsplash.com/premium_photo-1673264303561-de2ab31df03c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wx" }
