1dc89d6666de0eb7e6a0069c6f9913bfafe715ba
AmandaLesnik <lesnik.amanda@icloud.com>
2025-05-02 15:05:59 +0200

    add js page to contact

---
+++ assets/js/contact.js
@@ -0,0 +1,7 @@
+
+  // Contact form submission handler 
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent actual form submission
+  alert("Form has been submitted!");
+});
+
