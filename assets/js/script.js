4a13738d952a04e2847a59e3eea0f338348ec8a4
AmandaLesnik <lesnik.amanda@icloud.com>
2025-05-05 18:38:07 +0200

    Funktions work

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,4 +130,3 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent actual form submission
   alert("Form has been submitted!");
 });
-
